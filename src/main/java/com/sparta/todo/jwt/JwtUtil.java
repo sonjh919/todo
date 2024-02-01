8cab6976d0b63ee1e8f1236b5457c2dc37abde4c
Son JunHyung <kingjh1125@gmail.com>
2024-02-01 17:29:33 +0900

    refactor: add jwt exception

--- src/main/java/com/sparta/todo/jwt/JwtUtil.java
+++ src/main/java/com/sparta/todo/jwt/JwtUtil.java
@@ -1,29 +1,33 @@
 package com.sparta.todo.jwt;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
-import jakarta.servlet.http.HttpServletRequest;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
-import java.security.Key;
-import java.util.Base64;
-import java.util.Date;
-
 @Slf4j(topic = "JwtUtil")
 @Component
 public class JwtUtil {
 
-    // Header KEY 값
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-    // 사용자 권한 값의 KEY
-    // Token 식별자
     public static final String BEARER_PREFIX = "Bearer ";
-    // 토큰 만료시간
     private final long TOKEN_TIME = 60 * 60 * 1000L; // 60분
+
+    private final String INVALID_JWT_SIGNATURE = "Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.";
+    private final String EXPIRED_JWT_TOKEN = "Expired JWT token, 만료된 JWT token 입니다.";
+    private final String UNSUPPORTED_JWT_TOKEN = "Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.";
+    private final String EMPTY_JWT_CLAIMS = "JWT claims is empty, 잘못된 JWT 토큰 입니다.";
 
     @Value("${jwt.secret.key}") // Base64 Encode 한 SecretKey
     private String secretKey;
@@ -36,7 +40,6 @@ public class JwtUtil {
         key = Keys.hmacShaKeyFor(bytes);
     }
 
-    // 토큰 생성
     public String createToken(String username) {
         Date date = new Date();
 
@@ -49,34 +52,37 @@ public class JwtUtil {
                 .compact();
     }
 
-    // header 에서 JWT 가져오기
-    public String getJwtFromHeader(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+    public String getUserInfoFromToken(String bearerToken) {
+        String token = getJwtFromHeader(bearerToken);
+        if(validateToken((token))){
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
+        }
+        throw new IllegalArgumentException("오류");
+    }
+
+    private String getJwtFromHeader(String bearerToken) {
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
             return bearerToken.substring(7);
         }
         return null;
     }
 
-    // 토큰 검증
-    public boolean validateToken(String token) {
+    private boolean validateToken(String token) {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
         } catch (SecurityException | MalformedJwtException | SignatureException e) {
-            log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+            log.error(INVALID_JWT_SIGNATURE);
+            throw new JwtException(INVALID_JWT_SIGNATURE);
         } catch (ExpiredJwtException e) {
-            log.error("Expired JWT token, 만료된 JWT token 입니다.");
+            log.error(EXPIRED_JWT_TOKEN);
+            throw new JwtException(EXPIRED_JWT_TOKEN);
         } catch (UnsupportedJwtException e) {
-            log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+            log.error(UNSUPPORTED_JWT_TOKEN);
+            throw new JwtException(UNSUPPORTED_JWT_TOKEN);
         } catch (IllegalArgumentException e) {
-            log.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+            log.error(EMPTY_JWT_CLAIMS);
+            throw new JwtException(EMPTY_JWT_CLAIMS);
         }
-        return false;
-    }
-
-    // 토큰에서 사용자 정보 가져오기
-    public Claims getUserInfoFromToken(String token) {
-        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
     }
 }
