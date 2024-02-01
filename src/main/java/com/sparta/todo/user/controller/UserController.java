9893037add9e532dd8045b6f2ea40e37ef78047a
Son JunHyung <kingjh1125@gmail.com>
2024-02-01 15:22:32 +0900

    fix: edit log annotation

--- src/main/java/com/sparta/todo/user/controller/UserController.java
+++ src/main/java/com/sparta/todo/user/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j()
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserController {
