cbac528e6ca69b9181c58dc06137d47f79c91cd4
Son JunHyung <kingjh1125@gmail.com>
2024-02-04 16:56:34 +0900

    refactor: 공통로직 분리

--- src/main/java/com/sparta/todo/exception/ExceptionController.java
+++ src/main/java/com/sparta/todo/exception/ExceptionController.java
@@ -15,54 +15,45 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionController {
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionDto> ValidException(MethodArgumentNotValidException e) {
-        return ResponseEntity.badRequest().body(
-            new ExceptionDto(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST,
-                e.getBindingResult().getFieldError().getDefaultMessage()));
+    public ResponseEntity<ExceptionDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return createResponse(HttpStatus.BAD_REQUEST, e.getBindingResult().getFieldError().getDefaultMessage());
     }
 
     @ExceptionHandler(DuplicateKeyException.class)
-    public ResponseEntity<ExceptionDto> DuplicateKeyException(DuplicateKeyException e) {
-        return ResponseEntity.status(409)
-            .body(new ExceptionDto(HttpStatus.CONFLICT.value(), HttpStatus.CONFLICT,
-                e.getMessage()));
+    public ResponseEntity<ExceptionDto> handleDuplicateKeyException(DuplicateKeyException e) {
+        return createResponse(HttpStatus.CONFLICT, e.getMessage());
     }
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ExceptionDto> NoSuchElementException(NoSuchElementException e){
-        return ResponseEntity.badRequest().body(
-            new ExceptionDto(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST,
-                e.getMessage()));
+    public ResponseEntity<ExceptionDto> handleNoSuchElementException(NoSuchElementException e) {
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ExceptionDto> BadCredentialsException(BadCredentialsException e){
-        return ResponseEntity.badRequest().body(
-            new ExceptionDto(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST,
-                e.getMessage()));
+    public ResponseEntity<ExceptionDto> handleBadCredentialsException(BadCredentialsException e) {
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(JwtException.class)
-    public ResponseEntity<ExceptionDto> JwtException(JwtException e) {
-        return ResponseEntity.status(403)
-            .body(new ExceptionDto(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN,
-                e.getMessage()));
+    public ResponseEntity<ExceptionDto> handleJwtException(JwtException e) {
+        return createResponse(HttpStatus.FORBIDDEN, e.getMessage());
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ExceptionDto> EntityNotFoundException(EntityNotFoundException e) {
-        return ResponseEntity.badRequest()
-            .body(new ExceptionDto(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST,
-                e.getMessage()));
+    public ResponseEntity<ExceptionDto> handleEntityNotFoundException(EntityNotFoundException e) {
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<ExceptionDto> AccessDeniedException(AccessDeniedException e) {
-        return ResponseEntity.badRequest()
-            .body(new ExceptionDto(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST,
-                e.getMessage()));
+    public ResponseEntity<ExceptionDto> handleAccessDeniedException(AccessDeniedException e) {
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
+    private ResponseEntity<ExceptionDto> createResponse(HttpStatus status, String message) {
+        return ResponseEntity.status(status.value())
+            .body(new ExceptionDto(status.value(), status, message));
+    }
 
 }
