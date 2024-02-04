1cac65c38b145e611e52976a3122c70526da5067
Son JunHyung <kingjh1125@gmail.com>
2024-02-04 18:33:57 +0900

    refactor: 사용하지 않는 라이브러리 삭제

--- src/main/java/com/sparta/todo/user/entity/User.java
+++ src/main/java/com/sparta/todo/user/entity/User.java
@@ -3,8 +3,6 @@ package com.sparta.todo.user.entity;
 import com.sparta.todo.todo.entity.Todo;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
