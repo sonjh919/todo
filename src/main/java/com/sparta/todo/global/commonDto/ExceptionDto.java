476878d7ed5ae7e6b003ddc5e49654d6092631ef
Son JunHyung <kingjh1125@gmail.com>
2024-02-21 02:20:59 +0900

    refactor: 사용하지 않는 setter 제거

--- src/main/java/com/sparta/todo/global/commonDto/ExceptionDto.java
+++ src/main/java/com/sparta/todo/global/commonDto/ExceptionDto.java
@@ -2,11 +2,9 @@ package com.sparta.todo.global.commonDto;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@Setter
 @Builder
 public class ExceptionDto {
 
--- src/main/java/com/sparta/todo/global/commonDto/ResponseDto.java
+++ src/main/java/com/sparta/todo/global/commonDto/ResponseDto.java
@@ -2,10 +2,8 @@ package com.sparta.todo.global.commonDto;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Builder
 public class ResponseDto<T> {
 
