0f62c5db7c033eb365b3b966f31d15bb29dbbb55
Son JunHyung <kingjh1125@gmail.com>
2024-02-22 12:24:39 +0900

    test: user 테스트 추가
    1. 중복 유저일 경우 회원가입이 실패하는 테스트 추가
    2. 유저 이름이 잘못되었을 경우 조회를 실패하는 테스트 추가
    3. 패스워드가 다를 경우 로그인이 실패하는 테스트 추가

--- src/test/java/com/sparta/todo/domain/user/service/UserServiceTest.java
+++ src/test/java/com/sparta/todo/domain/user/service/UserServiceTest.java
@@ -1,7 +1,7 @@
 package com.sparta.todo.domain.user.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.sparta.todo.domain.fixture.TestUser;
 import com.sparta.todo.domain.user.dto.LoginRequestDto;
@@ -10,59 +10,124 @@ import com.sparta.todo.domain.user.dto.SignupResponseDto;
 import com.sparta.todo.domain.user.repository.UserRepository;
 import com.sparta.todo.global.jwt.JwtUtil;
 import com.sparta.todo.mock.MockPasswordEncoder;
+import com.sparta.todo.mock.MockUserRepository;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class UserServiceTest {
-
-    @Mock
-    UserRepository userRepository;
+    private UserService userService;
+    private UserRepository userRepository;
     @Mock
     JwtUtil jwtUtil;
-    PasswordEncoder passwordEncoder = new MockPasswordEncoder();
+
+    private static final Long USER_ID = 1L;
     private static final String USERNAME = "sjh";
+    private static final String NONE_USERNAME = "sjh1";
     private static final String PASSWORD = "12345678";
+    private static final String WRONG_PASSWORD = "123456789";
 
-    @Test
-    void signup_회원가입_성공() {
-        //given
-        SignupRequestDto signupRequestDto = new SignupRequestDto();
-        ReflectionTestUtils.setField(signupRequestDto, "userName", USERNAME);
-        ReflectionTestUtils.setField(signupRequestDto, "password", PASSWORD);
+    @BeforeEach
+    void init(){
+        userRepository = new MockUserRepository();
+        PasswordEncoder passwordEncoder = new MockPasswordEncoder();
 
-        UserService userService = new UserService(userRepository, passwordEncoder, jwtUtil);
-
-        //when
-        SignupResponseDto signupResponseDto = userService.signup(signupRequestDto);
-
-        //then
-        assertEquals(USERNAME, signupResponseDto.getUserName());
-        assertEquals(PASSWORD, signupRequestDto.getPassword());
+        userService = new UserService(userRepository, passwordEncoder, jwtUtil);
     }
 
-    @Test
-    void login_로그인_성공() {
-        //given
-        LoginRequestDto loginRequestDto = new LoginRequestDto();
-        ReflectionTestUtils.setField(loginRequestDto, "userName", USERNAME);
-        ReflectionTestUtils.setField(loginRequestDto, "password", PASSWORD);
+    @Nested
+    class signup_회원가입_테스트{
+        @Test
+        void 회원가입_성공() {
+            //given
+            SignupRequestDto signupRequestDto = new SignupRequestDto();
+            setDto(signupRequestDto, USERNAME, PASSWORD);
 
-        TestUser user = new TestUser();
-        given(userRepository.userBy(USERNAME)).willReturn(user);
-        UserService userService = new UserService(userRepository, passwordEncoder, jwtUtil);
+            //when
+            SignupResponseDto signupResponseDto = userService.signup(signupRequestDto);
 
-        //when
-        String token = userService.login(loginRequestDto);
+            //then
+            assertEquals(USER_ID, signupResponseDto.getId());
+            assertEquals(USERNAME, signupResponseDto.getUserName());
+            assertEquals(PASSWORD, signupRequestDto.getPassword());
+        }
 
-        //then
-        assertEquals(user.createToken(jwtUtil), token);
+        @Test
+        void 중복유저_회원가입_실패(){
+            //given
+            SignupRequestDto signupRequestDto = new SignupRequestDto();
+            setDto(signupRequestDto, USERNAME, PASSWORD);
+
+            userService.signup(signupRequestDto);
+
+            //when - then
+            assertThrows(DuplicateKeyException.class, () -> userService.signup(signupRequestDto));
+        }
+
     }
+
+    @Nested
+    class login_로그인_테스트 {
+        @Test
+        void 로그인_성공() {
+            //given
+            SignupRequestDto signupRequestDto = new SignupRequestDto();
+            setDto(signupRequestDto, USERNAME, PASSWORD);
+            userService.signup(signupRequestDto);
+
+            LoginRequestDto loginRequestDto = new LoginRequestDto();
+            setDto(loginRequestDto, USERNAME, PASSWORD);
+
+            //when
+            String token = userService.login(loginRequestDto);
+
+            //then
+            assertEquals(new TestUser().createToken(jwtUtil), token);
+        }
+
+        @Test
+        void 사용자_조회_실패() {
+            //given
+            SignupRequestDto signupRequestDto = new SignupRequestDto();
+            setDto(signupRequestDto, USERNAME, PASSWORD);
+            userService.signup(signupRequestDto);
+
+            LoginRequestDto loginRequestDto = new LoginRequestDto();
+            setDto(loginRequestDto, NONE_USERNAME, PASSWORD);
+
+            //when-then
+            assertThrows(NoSuchElementException.class, () -> userService.login(loginRequestDto));
+        }
+
+        @Test
+        void 패스워드_불일치_로그인_실패() {
+            //given
+            SignupRequestDto signupRequestDto = new SignupRequestDto();
+            setDto(signupRequestDto, USERNAME, PASSWORD);
+            userService.signup(signupRequestDto);
+
+            LoginRequestDto loginRequestDto = new LoginRequestDto();
+            setDto(loginRequestDto, USERNAME, WRONG_PASSWORD);
+
+            assertThrows(BadCredentialsException.class, () -> userService.login(loginRequestDto));
+        }
+    }
+
+    private <T> void setDto(T dto, String userName, String password) {
+        ReflectionTestUtils.setField(dto, "userName", userName);
+        ReflectionTestUtils.setField(dto, "password", password);
+    }
+
 }
